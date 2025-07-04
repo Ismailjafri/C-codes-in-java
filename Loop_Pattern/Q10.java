1324105fcdab3755390880161be4466079b30ceb
Ismailjafri <142569275+Ismailjafri@users.noreply.github.com>
2025-07-04 09:50:33 +0530

    Update Q10.java

--- Loop_Pattern/Q10.java
+++ Loop_Pattern/Q10.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Q10 {
     public static void main(String args[]){
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter The Size Of The Pattern: ");
+        System.out.print("Enter The Size Of The Pattern Rn: ");
         int a = input.nextInt();
 
         for (int i = 1; i <= a ; i++) {
